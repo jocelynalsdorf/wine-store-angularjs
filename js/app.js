7da97deeac14fb399f8b722bc6859d3268c74cb6
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-28 14:11:52 -0700

    add ajax to controller; wine items displaying

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 var app = angular.module("wineApp", []);
+
+app.controller("wineCtrl", function($scope, $http){
+  $scope.wineList = [];
+  
+    $http.get("http://daretodiscover.herokuapp.com/wines")
+      .success(function(wines){
+        $scope.wines = wines;
+      })
+      .error(function(){
+        alert("something went wrong");
+      })
+});
