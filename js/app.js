0cbf4c067a781af370e8e27fbab5a8b02307fd15
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-29 11:13:46 -0700

    add ajax post request to add wine and hooks up inputs to the  model

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@ var app = angular.module("wineApp", []);
 
 app.controller("wineCtrl", function($scope, $http){
   $scope.wineList = [];
-  
+  $scope.saveWine = function(){
+    // $scope.wineList.push({name: $scope.name, year: $scope.year, grapes: $scope.grapes, region: $scope.region, price: $scope.price, image: $scope.image, description: $scope.description});
+
+  console.log($scope.wine);
+     $http.post("http://daretodiscover.herokuapp.com/wines", $scope.wine)
+        .then(function(){
+        $("#add-wine-modal").modal("hide");
+        location.reload();
+      });
+  }
     $http.get("http://daretodiscover.herokuapp.com/wines")
       .success(function(wines){
         $scope.wines = wines;
@@ -10,4 +19,12 @@ app.controller("wineCtrl", function($scope, $http){
       .error(function(){
         alert("something went wrong");
       })
+
+     
+  // }
+      
+ 
+
+
+
 });
